18ee68dc0d0eb1851629a750fcb71ff72066bbe0
Samuel Eras <sa.eras@gmx.de>
2024-01-05 18:15:10 +0100

    add Searchbar

---
+++ src/components/SearchBar.tsx
@@ -0,0 +1,24 @@
+import { SearchIcon } from "@chakra-ui/icons";
+import { InputGroup, InputLeftElement, Input } from "@chakra-ui/react";
+
+const SearchBar = () => {
+  return (
+    <InputGroup>
+      <InputLeftElement pointerEvents="none" h="100%">
+        <SearchIcon color="gray.100" boxSize={5} />
+      </InputLeftElement>
+      <Input
+        h="100%"
+        flex="1"
+        bg="gray.500"
+        borderRadius="2rem"
+        border="none"
+        type="text"
+        placeholder="Search games..."
+        _placeholder={{ color: "gray.100" }}
+      />
+    </InputGroup>
+  );
+};
+
+export default SearchBar;
