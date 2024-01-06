d6b5833dc9ea636c8cdd112419e4ac034d45944a
Samuel Eras <sa.eras@gmx.de>
2024-01-06 12:16:50 +0100

    change game-service to custom hook useGames

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,33 +1,15 @@
-import { useEffect, useState } from "react";
-import gamesService, { Game } from "../services/games-service";
-import { CanceledError } from "axios";
 import { Text } from "@chakra-ui/react";
+import useGames, { Game } from "../hooks/useGames";
 
 const GameGrid = () => {
-  const [games, setGames] = useState<Game[]>([]);
-  const [error, setError] = useState("");
-
-  useEffect(() => {
-    const { request, cancel } = gamesService.getAllGames();
-    request
-      .then((res) => {
-        console.log(res.data.results);
-        setGames(res.data.results);
-      })
-      .catch((err) => {
-        if (err instanceof CanceledError) return;
-        setError(err.message);
-      });
-
-    return () => cancel();
-  }, []); //Loads once
+  const { games, error } = useGames();
 
   return (
     <>
       <div>GameGrid</div>
       {error && <Text color={"red.400"}>{error}</Text>}
       <ul>
-        {games.map((game) => (
+        {games.map((game: Game) => (
           <li key={game.id}>{game.name}</li>
         ))}
       </ul>
