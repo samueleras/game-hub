09f25400da79c119393668d119c2c7b1e26c8958
Samuel Eras <sa.eras@gmx.de>
2024-01-08 21:10:13 +0100

    adjust css width

--- src/components/GameCardSkeleton.tsx
+++ src/components/GameCardSkeleton.tsx
@@ -2,9 +2,9 @@ import { Card, CardBody, Skeleton, SkeletonText } from "@chakra-ui/react";
 
 const GameCardSkeleton = () => {
   return (
-    <Card maxW="100%" overflow={"hidden"} width="100%">
+    <Card maxW="100%" overflow={"hidden"}>
       <Skeleton>
-        <img src="" alt="" height={"400px"} width={"600px"} />
+        <img src="" alt="" width={"600px"} height={"400px"} />
       </Skeleton>
       <CardBody>
         <SkeletonText mt="4" noOfLines={4} spacing="4" skeletonHeight="2" />
