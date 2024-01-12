210da01942a1e8fa46f84c38bd52153f538248a1
Samuel Eras <sa.eras@gmx.de>
2024-01-12 14:32:10 +0100

    fetch and display genres

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Grid, GridItem } from "@chakra-ui/react";
 import NavigationBar from "./components/NavigationBar";
 import GameGrid from "./components/GameGrid";
+import GenreList from "./components/GenreList";
 
 function App() {
   return (
@@ -22,7 +23,7 @@ function App() {
         <NavigationBar />
       </GridItem>
       <GridItem area={"aside"} display={{ base: "none", lg: "block" }}>
-        Aside
+        <GenreList />
       </GridItem>
       <GridItem area={"main"}>
         <GameGrid />
