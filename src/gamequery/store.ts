ac3048d6ee67522dd5648d66ccf76e9db0727eb2
Samuel Eras <sa.eras@gmx.de>
2024-02-11 08:58:47 +0100

    create gameQuery store with zustand

---
+++ src/gamequery/store.ts
@@ -0,0 +1,36 @@
+import { create } from "zustand";
+
+export interface GameQuery {
+  genreId?: number;
+  platformId?: number;
+  sortOrder?: string;
+  searchText?: string;
+  pageSize?: number;
+}
+
+interface GameQueryStore {
+  gameQuery: GameQuery;
+  setGenreId: (genreId: number) => void;
+  setPlatformId: (platformId: number) => void;
+  setSortOrder: (sortOrder: string) => void;
+  setSearchText: (searchText: string) => void;
+  setPageSize: (pageSize: number) => void;
+}
+
+const useGameQueryStore = create<GameQueryStore>((set) => ({
+  gameQuery: {} as GameQuery,
+  setGenreId: (genreId) =>
+    set((store) => ({ gameQuery: { ...store.gameQuery, genreId } })),
+  setPlatformId: (platformId) =>
+    set((store) => ({ gameQuery: { ...store.gameQuery, platformId } })),
+  setSortOrder: (sortOrder) =>
+    set((store) => ({ gameQuery: { ...store.gameQuery, sortOrder } })),
+  setSearchText: (searchText) =>
+    set((store) => ({
+      gameQuery: { searchText, pageSize: store.gameQuery.pageSize },
+    })),
+  setPageSize: (pageSize) =>
+    set((store) => ({ gameQuery: { ...store.gameQuery, pageSize } })),
+}));
+
+export default useGameQueryStore;
