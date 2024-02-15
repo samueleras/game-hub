6eddf804bc804c7243b9b44d1d886d9f32f77bab
Samuel Eras <sa.eras@gmx.de>
2024-02-15 17:20:17 +0100

    add deselect genre button

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,7 +1,8 @@
-import { Button, Flex, Heading, Image, Spinner } from "@chakra-ui/react";
+import { Box, Button, Flex, Heading, Image, Spinner } from "@chakra-ui/react";
 import useGameQueryStore from "../gamequery/store";
 import useGenres from "../hooks/useGenres";
 import getCroppedImageUrl from "../services/image-url";
+import { CloseIcon } from "@chakra-ui/icons";
 
 const GenreList = () => {
   const { data: genres, error, isLoading } = useGenres();
@@ -17,32 +18,41 @@ const GenreList = () => {
         Genres
       </Heading>
       {genres?.results.map((genre) => (
-        <Flex
-          key={genre.id}
-          alignItems={"center"}
-          gap={3}
-          marginTop={3}
-          borderRadius="0.5rem"
-        >
-          <Image
-            boxSize="32px"
-            borderRadius="0.5rem"
-            objectFit="cover"
-            src={getCroppedImageUrl(genre.image_background)}
-            alt={genre.name}
-          />
-          <Button
-            fontSize="lg"
-            variant="link"
-            whiteSpace="normal"
-            textAlign="left"
-            textDecoration={selectedGenreId === genre.id ? "underline" : "none"}
-            fontWeight={selectedGenreId === genre.id ? "bold" : "normal"}
-            onClick={() => setSelectedGenreId(genre.id)}
+        <>
+          <Flex
+            key={genre.id}
+            alignItems={"center"}
+            marginTop={3}
+            gap={3}
+            justifyContent="space-between"
           >
-            {genre.name}
-          </Button>
-        </Flex>
+            <Flex gap={3} alignItems={"center"}>
+              <Image
+                boxSize="32px"
+                borderRadius="0.5rem"
+                objectFit="cover"
+                src={getCroppedImageUrl(genre.image_background)}
+                alt={genre.name}
+              />
+              <Button
+                fontSize="lg"
+                variant="link"
+                whiteSpace="normal"
+                textAlign="left"
+                textDecoration={
+                  selectedGenreId === genre.id ? "underline" : "none"
+                }
+                fontWeight={selectedGenreId === genre.id ? "bold" : "normal"}
+                onClick={() => setSelectedGenreId(genre.id)}
+              >
+                {genre.name}
+              </Button>
+            </Flex>
+            {selectedGenreId === genre.id && (
+              <CloseIcon onClick={() => setSelectedGenreId(undefined)} />
+            )}
+          </Flex>
+        </>
       ))}
     </>
   );
